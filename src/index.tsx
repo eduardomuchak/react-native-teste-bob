1cb65c0d57ab5baf4bc88f82950f0f1c6e5a4ec4
Eduardo <eduardomuchak@gmail.com>
2024-03-06 16:47:05 -0300

    fix: adiciona toast export

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 export * from './components/button';
 export * from './components/text';
+export * from './components/toast';
 
 export function multiply(a: number, b: number): Promise<number> {
   return Promise.resolve(a + b);
