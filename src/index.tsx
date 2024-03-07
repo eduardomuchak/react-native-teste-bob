4f12058cc73de08f7a1eba80f457729f32ed3f5b
Eduardo <eduardomuchak@gmail.com>
2024-03-07 11:14:02 -0300

    chore: adiciona export theme

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,4 @@
 export * as icons from './assets/icons'
 export * as components from './components'
 export * as unistyles from './lib/unistyles'
+export { defaultTheme } from './lib/unistyles/themes'
