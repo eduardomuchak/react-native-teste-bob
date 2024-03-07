66c4e264bb07690d27a29faf64b79d77434d3aca
Eduardo <eduardomuchak@gmail.com>
2024-03-07 11:20:41 -0300

    fix: ajusta export theme

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,4 @@
 export * as icons from './assets/icons'
 export * as components from './components'
 export * as unistyles from './lib/unistyles'
-export { defaultTheme } from './lib/unistyles/themes'
+export * from './lib/unistyles/themes'
